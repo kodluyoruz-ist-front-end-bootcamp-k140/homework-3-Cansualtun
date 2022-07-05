465a7b5aa75912010d0cf1f7ec252dbe786d9580
CansuAltun <altuncansu98@gmail.com>
2022-07-05 05:21:18 +0300

    commitfirst

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav class="navbar bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1" style={{marginLeft:550}}>K-140 Kodluyoruz Bootcamp Homework 3</span>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar ;
