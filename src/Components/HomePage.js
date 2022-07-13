2a5a52562b674ac17374e387ad0045bc055ab83d
CansuAltun <altuncansu98@gmail.com>
2022-07-13 14:39:22 +0300

    homework 3-end

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,36 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import "../App.css";
 import ToDo from "./ToDo";
 import Navbar from "./Navbar";
 import { Posts } from "./Posts";
 import { Button } from "./Button/Button";
+import ReactSwitch from "react-switch";
+
 //Oluşturulan Componentler tek tek render edilerek home pagede olarak tek componenet tasarımı amaçlanmıştır.
 
+export const ThemeContext = createContext("null");
 function HomePage() {
   const [activeTab, setActiveTab] = useState("todo");
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
     <>
-      <Navbar />
-      <div className="App">
-        <div className="btn-group tabs" role="group" aria-label="Basic-example">
-          <Button
-            onClick={() => setActiveTab("post")}
-            className={
-              activeTab === "post" ? "btn btn-primary" : "btn btn-default"
-            }
-          >
-            Posts
-          </Button>
-          <Button
-            onClick={() => setActiveTab("todo")}
-            className={
-              activeTab === "todo" ? "btn btn-primary" : "btn btn-default"
-            }
-          >
-            Todo
-          </Button>
-        </div>
-        {activeTab === "todo" ? <ToDo /> : <Posts />}
+      <div className="App" id={theme}>
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+          <div className="App" id={theme}>
+            <div className="switch">
+              <label> {theme === "light" ? "Light Mode" : "Dark Mode"} </label>
+              <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+            </div>
+            <Navbar />
+            <div
+              className="btn-group tabs"
+              role="group"
+              aria-label="Basic-example"
+            >
+              <Button
+                onClick={() => setActiveTab("post")}
+                className={
+                  activeTab === "post" ? "btn btn-primary" : "btn btn-default"
+                }
+              >
+                Posts
+              </Button>
+              <Button
+                onClick={() => setActiveTab("todo")}
+                className={
+                  activeTab === "todo" ? "btn btn-primary" : "btn btn-default"
+                }
+              >
+                Todo
+              </Button>
+            </div>
+            {activeTab === "todo" ? <ToDo /> : <Posts />}
+          </div>
+        </ThemeContext.Provider>
       </div>
     </>
   );
